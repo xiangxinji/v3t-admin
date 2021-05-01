97db5070c044c1355c3b626203ef68ed0bdb24b8
xiangxinji <xxjbbking@163.com>
2021-05-01 12:36:42 +0800

    remove

--- src/hooks/states.ts
+++
@@ -1,8 +0,0 @@
-import deepClone from 'deepclone';
-
-export function useResetCallback(state : any, resetKey : string) {
-  const temp = deepClone(state[resetKey]);
-  return function reset() {
-    state[resetKey] = deepClone(temp);
-  };
-}
