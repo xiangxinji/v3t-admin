4a03cfaa9fc706e5a848776f00303826075fd85d
xiangxinji <xxjbbking@163.com>
2021-06-21 16:38:22 +0800

    fix: 修复部分问题

--- src/components/lazy.use.ts
+++ src/components/lazy.use.ts
@@ -21,24 +21,29 @@ import {
   ElPagination,
 } from 'element-plus';
 
+const components:Array<{name : string}> = [
+  ElButton,
+  ElContainer,
+  ElHeader,
+  ElMain,
+  ElAside,
+  ElBreadcrumb,
+  ElIcon,
+  ElMenu,
+  ElSubmenu,
+  ElMenuItem,
+  ElBreadcrumbItem,
+  ElInput,
+  ElTable,
+  ElTableColumn,
+  ElMessageBox,
+  ElPagination,
+];
 export default {
   install(app : App) : App {
-    app.component(ElButton.name, ElButton);
-    app.component(ElContainer.name, ElContainer);
-    app.component(ElHeader.name, ElHeader);
-    app.component(ElMain.name, ElMain);
-    app.component(ElAside.name, ElAside);
-    app.component(ElBreadcrumb.name, ElBreadcrumb);
-    app.component(ElIcon.name, ElIcon);
-    app.component(ElMenu.name, ElMenu);
-    app.component(ElMenuItem.name, ElMenuItem);
-    app.component(ElSubmenu.name, ElSubmenu);
-    app.component(ElBreadcrumbItem.name, ElBreadcrumbItem);
-    app.component(ElInput.name, ElInput);
-    app.component(ElTable.name, ElTable);
-    app.component(ElTableColumn.name, ElTableColumn);
-    app.component(ElMessageBox.name, ElMessageBox);
-    app.component(ElPagination.name, ElPagination);
+    components.forEach((component) => {
+      app.component(component.name, component);
+    });
     return app;
   },
 };
